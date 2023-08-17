2b86d14974067978abbeb55a131b79993c16fadc
EmiGarBar <110506022+EmiGarBar@users.noreply.github.com>
2023-08-16 18:34:49 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump=loadSound("jump.wav");
+	mario_coin=loadSound("coin.wav");
+	mario_kick=loadSound("kick.wav");
+	mario_die=loadSound("mariodie.wav");
+	mario_gameover=loadSound("gameover.wav");
+
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	instializeInSetup(mario);
+
+	video=createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+
+	poseNet=ml5.poseNet(video,modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+	console.log('modelo cargado')
+}
+
+function  gotPoses (results){
+	if(results.lenght>0)
+	{
+		console.log(results);
+		noseX=reults[0].pose.nose.x;
+		noseY=reults[0].pose.nose.y;
+
+	}
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
